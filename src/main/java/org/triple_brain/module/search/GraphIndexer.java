0635fbe478e24653acb1b51ccc4f11721f211118
Vincent Blouin <vincent.blouin@gmail.com>
2013-09-20 17:17:28 -0400

    trying to fix cannot reindex edges when reindexing whole graph

--- src/main/java/org/triple_brain/module/search/GraphIndexer.java
+++ src/main/java/org/triple_brain/module/search/GraphIndexer.java
@@ -12,7 +12,10 @@ import org.triple_brain.module.model.graph.GraphElement;
 import org.triple_brain.module.model.graph.Vertex;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 
 import static org.triple_brain.module.common_utils.Uris.encodeURL;
 
@@ -25,6 +28,7 @@ public class GraphIndexer {
     @Inject
     WholeGraph wholeGraph;
 
+    private int INDEX_AFTER_HOW_NB_DOCUMENTS = 100;
     private CoreContainer coreContainer;
     private SearchUtils searchUtils;
 
@@ -37,34 +41,17 @@ public class GraphIndexer {
         this.searchUtils = SearchUtils.usingCoreCoreContainer(coreContainer);
     }
 
-    private String configFilePath() {
-        return coreContainer.getConfigFile().getParent() + "/" +
-                coreContainer.getCore(coreContainer.getDefaultCoreName()).getCoreDescriptor().getConfigName();
-    }
-
-    private String schemaFilePath() {
-        return coreContainer.getConfigFile().getParent() + "/" +
-                coreContainer.getCore(coreContainer.getDefaultCoreName()).getCoreDescriptor().getSchemaName();
-    }
-
     public void indexWholeGraph(){
+        indexAllVertices();
         indexAllEdges();
     }
 
     public void indexVertex(Vertex vertex) {
         try {
-            SolrInputDocument document = graphElementToDocument(vertex);
-            document.addField("is_vertex", true);
-            document.addField("is_public", vertex.isPublic());
-            document.addField("comment", vertex.comment());
-            for (Edge edge : vertex.connectedEdges()) {
-                document.addField(
-                        "relation_name",
-                        edge.label()
-                );
-            }
             SolrServer solrServer = searchUtils.getServer();
-            solrServer.add(document);
+            solrServer.add(
+                    vertexDocument(vertex)
+            );
             solrServer.commit();
         } catch (IOException | SolrServerException e) {
             throw new RuntimeException(e);
@@ -73,18 +60,10 @@ public class GraphIndexer {
 
     public void indexRelation(Edge edge) {
         try {
-            SolrInputDocument document = graphElementToDocument(edge);
-            document.addField("is_vertex", false);
-            document.addField(
-                    "source_vertex_uri",
-                    encodeURL(edge.sourceVertex().uri())
-            );
-            document.addField(
-                    "destination_vertex_uri",
-                    encodeURL(edge.destinationVertex().uri())
-            );
             SolrServer solrServer = searchUtils.getServer();
-            solrServer.add(document);
+            solrServer.add(
+                    edgeDocument(edge)
+            );
             solrServer.commit();
         } catch (IOException | SolrServerException e) {
             throw new RuntimeException(e);
@@ -109,24 +88,89 @@ public class GraphIndexer {
         coreContainer.shutdown();
     }
 
-    private void indexAllVertices(){
-        Iterator<Vertex> vertexIt = wholeGraph.getAllVertices();
-        while(vertexIt.hasNext()){
-            indexVertex(
-                    vertexIt.next()
+    private SolrInputDocument edgeDocument(Edge edge){
+        SolrInputDocument document = graphElementToDocument(edge);
+        document.addField("is_vertex", false);
+        document.addField(
+                "source_vertex_uri",
+                encodeURL(edge.sourceVertex().uri())
+        );
+        document.addField(
+                "destination_vertex_uri",
+                encodeURL(edge.destinationVertex().uri())
+        );
+        return document;
+    }
+
+    private SolrInputDocument vertexDocument(Vertex vertex){
+        SolrInputDocument document = graphElementToDocument(vertex);
+        document.addField("is_vertex", true);
+        document.addField("is_public", vertex.isPublic());
+        document.addField("comment", vertex.comment());
+        for (Edge edge : vertex.connectedEdges()) {
+            document.addField(
+                    "relation_name",
+                    edge.label()
             );
         }
+        return document;
+    }
+
+    private void indexAllVertices(){
+        Iterator<Vertex> vertexIt = wholeGraph.getAllVertices();
+        Set<SolrInputDocument> vertexDocuments = new HashSet<>();
+        int totalIndexed = 0;
+        int nbInCycle = 0;
+        while(vertexIt.hasNext()){
+            vertexDocuments.add(
+                    vertexDocument(vertexIt.next())
+            );
+            nbInCycle++;
+            if(nbInCycle == INDEX_AFTER_HOW_NB_DOCUMENTS){
+                totalIndexed += INDEX_AFTER_HOW_NB_DOCUMENTS;
+                System.out.println(
+                        "Indexing vertices ... total indexed " + totalIndexed
+                );
+                addDocumentsAndCommit(vertexDocuments);
+                nbInCycle = 0;
+                vertexDocuments = new HashSet<>();
+            }
+        }
+        addDocumentsAndCommit(vertexDocuments);
     }
 
     private void indexAllEdges(){
         Iterator<Edge> edgeIt = wholeGraph.getAllEdges();
+        Set<SolrInputDocument> edgesDocument = new HashSet<>();
+        int totalIndexed = 0;
+        int nbInCycle = 0;
         while(edgeIt.hasNext()){
-            indexRelation(
-                    edgeIt.next()
+            edgesDocument.add(
+                edgeDocument(edgeIt.next())
             );
+            nbInCycle++;
+            if(nbInCycle == INDEX_AFTER_HOW_NB_DOCUMENTS){
+                totalIndexed += INDEX_AFTER_HOW_NB_DOCUMENTS;
+                System.out.println(
+                        "Indexing edges ... total indexed " + totalIndexed
+                );
+                addDocumentsAndCommit(edgesDocument);
+                nbInCycle = 0;
+                edgesDocument = new HashSet<>();
+            }
         }
+        addDocumentsAndCommit(edgesDocument);
     }
 
+    private void addDocumentsAndCommit(Collection<SolrInputDocument> collection){
+        try{
+            SolrServer solrServer = searchUtils.getServer();
+            solrServer.add(collection);
+            solrServer.commit();
+        }catch(SolrServerException | IOException e){
+            throw new RuntimeException(e);
+        }
+    }
     private SolrInputDocument graphElementToDocument(GraphElement graphElement) {
         SolrInputDocument document = new SolrInputDocument();
         document.addField("uri", encodeURL(graphElement.uri()));
