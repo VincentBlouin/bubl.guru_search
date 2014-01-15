07b4c256facfd228e3665ef6cb2e3ac0762dfc1b
Vincent Blouin <vincent.blouin@gmail.com>
2014-01-15 07:53:55 -0500

    refactor to prepare performance improvements

--- src/main/java/org/triple_brain/module/search/GraphIndexer.java
+++ src/main/java/org/triple_brain/module/search/GraphIndexer.java
@@ -4,13 +4,13 @@ package org.triple_brain.module.search;
 * Copyright Mozilla Public License 1.1
 */
 
-import org.triple_brain.module.model.graph.Edge;
 import org.triple_brain.module.model.graph.GraphElement;
-import org.triple_brain.module.model.graph.Vertex;
+import org.triple_brain.module.model.graph.edge.Edge;
+import org.triple_brain.module.model.graph.vertex.VertexOperator;
 
 public interface GraphIndexer {
     public void indexWholeGraph();
-    public void indexVertex(Vertex vertex);
+    public void indexVertex(VertexOperator vertex);
     public void indexRelation(Edge edge);
     public void deleteGraphElement(GraphElement graphElement);
     public void handleEdgeLabelUpdated(Edge edge);
